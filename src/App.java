18df44be59dde9df5dd1bfe6c69b9bfd5f05f9c4
Nadine Giasson St-Amand <stamandnadine@cgmatane.qc.ca>
2020-09-19 10:41:06 -0400

    Parsing liste nouvelle

--- src/App.java
+++ src/App.java
@@ -1,10 +1,22 @@
 
-import vue.Fenetre;
+import java.util.List;
+
+import donnee.NouvelleDAO;
+//import vue.Fenetre;
+import modele.Nouvelle;
 
 public class App {
 
 	public static void main(String[] parametres) {
-		Fenetre.launch(Fenetre.class, parametres);	
+		
+		NouvelleDAO nouvelleDAO = new NouvelleDAO();
+		List<Nouvelle> nouvelles = nouvelleDAO.listerNouvelles();
+		for(Nouvelle nouvelle : nouvelles)
+		{
+			System.out.println(nouvelle.getTitre());
+		}
+		
+		//Fenetre.launch(Fenetre.class, parametres);	
 	}
 
 }
--- src/donnee/NouvelleDAO.java
+++ src/donnee/NouvelleDAO.java
@@ -1,11 +1,20 @@
 package donnee;
 
 import java.io.InputStream;
+import java.io.StringBufferInputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 import modele.Nouvelle;
 
@@ -33,6 +42,32 @@ public class NouvelleDAO {
 		}
 
 		System.out.println(xml);
+		
+		try {
+			DocumentBuilder parseur = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			Document document = parseur.parse(new StringBufferInputStream(xml));
+			
+			NodeList noeudsItems = document.getElementsByTagName("item");
+			
+			for(int position = 0; position < noeudsItems.getLength(); position++)
+			{
+				Element item = (Element)noeudsItems.item(position);
+				System.out.println(item);
+				String lien = item.getElementsByTagName("link").item(0).getTextContent();
+				String titre = item.getElementsByTagName("title").item(0).getTextContent();
+				String description = item.getElementsByTagName("description").item(0).getTextContent();
+				String date = item.getElementsByTagName("pubDate").item(0).getTextContent();
+				//System.out.println(titre);
+				
+				Nouvelle nouvelle = new Nouvelle(lien, titre, description, date);
+				//System.out.println(nouvelle.getTitre() + " " + nouvelle.getLien());
+				nouvelles.add(nouvelle);	
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		
 		return nouvelles;
 	}
